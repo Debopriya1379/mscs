add326d9326b6f28f38cba21bd08435804cb3529
Debopriya1379 <papai7909@gmail.com>
2023-06-17 17:21:32 +0530

    clean up

--- src/componenets/RightUp.jsx
+++ src/componenets/RightUp.jsx
@@ -1,20 +1,20 @@
-import React, { useState,useEffect } from 'react'
-import {data} from './data.js'
-import { Pie } from "react-chartjs-2";
+import React from 'react'
+// import {data} from './data.js'
+// import { Pie } from "react-chartjs-2";
 
 export const RightUp = ({entryCountPerYear}) => {
 
   // const [Years,setYears] = useState(data.map((item)=> {return parseInt(item.dateofreg.substring(6)) }))
 
-  const[graphData, setGraphData] = useState({
-    labels : Object.keys(entryCountPerYear),
-    dataset : [
-      {
-        label : "joined",
-        data : Object.values(entryCountPerYear),
-      },
-    ],
-  });
+  // const[graphData, setGraphData] = useState({
+  //   labels : Object.keys(entryCountPerYear),
+  //   dataset : [
+  //     {
+  //       label : "joined",
+  //       data : Object.values(entryCountPerYear),
+  //     },
+  //   ],
+  // });
 
   return (
     <div>
